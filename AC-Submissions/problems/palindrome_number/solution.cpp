169c84324ee2b4fa7b396419482157e5920d5af7
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2020-12-31 13:35:55 +0000

    Sync LeetCode submission - Palindrome Number (cpp)

---
+++ AC-Submissions/problems/palindrome_number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int n = abs(x);
+        long long ans = 1, rev = 0; 
+        while (n){
+            int rem = n % 10;
+            n /=  10;
+            rev = rev * 10LL + rem;
+        }
+        if(rev == x){
+            return true;
+        }
+        else return false;
+    }
+};
