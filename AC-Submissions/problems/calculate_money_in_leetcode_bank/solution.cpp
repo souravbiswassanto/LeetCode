bc0ee2e3fd2038ff26776912efc5b65cec251754
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2021-01-09 14:46:51 +0000

    Sync LeetCode submission - Calculate Money in Leetcode Bank (cpp)

---
+++ AC-Submissions/problems/calculate_money_in_leetcode_bank/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long totalMoney(int n) {
+        long long cntr = 0, sum = 0;
+        for(int i = 0;i < n; i++){
+            if(i % 7 == 0)cntr++;
+            sum += ( i % 7 + cntr);
+        }
+        return sum;
+    }
+};
