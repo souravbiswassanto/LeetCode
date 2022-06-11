2a75426293a7dbcb4a50f35ea84f2ced7f188c60
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-06-11 14:54:37 +0000

    Sync LeetCode submission - Count Subarrays With Score Less Than K (cpp)

---
+++ AC-Submissions/problems/count_subarrays_with_score_less_than_k/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int l= 0;
+        int n = nums.size();
+        long long ans = 0, sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            while(l <= i and (sum * 1LL * (i - l + 1)) >= k){
+                sum -= nums[l];
+                l++;
+            }
+            if(l <= i){
+                ans += (i - l + 1);
+            }
+        }
+        return ans;
+    }
+};
