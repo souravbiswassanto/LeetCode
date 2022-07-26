a6df0586410e2a19f116efd319a17009c1204fe5
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-07-26 10:16:34 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (cpp)

---
+++ AC-Submissions/problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        TreeNode* answer; 
+        bool isFoundBoth = false;
+        function<bool(TreeNode*)> dfs = [&](TreeNode* cur){
+            if (cur == NULL) return false;
+            bool isFoundP = false;
+            bool isFoundQ = false;
+            
+            isFoundP = dfs(cur->left);
+            isFoundQ = dfs(cur->right);
+    
+            if (isFoundP and isFoundQ) {
+                if (isFoundBoth == false) {
+                    isFoundBoth = true;
+                    answer = cur;
+                }
+            }
+            if((isFoundP | isFoundQ) and (cur == p or cur == q)){
+                if (isFoundBoth == false) {
+                    isFoundBoth = true;
+                    answer = cur;
+                }
+            }
+            if(p == cur or q == cur) return true;
+            return bool(isFoundP | isFoundQ);
+            
+        };
+        bool dummy = dfs(root);
+        return answer;
+    }
+};
