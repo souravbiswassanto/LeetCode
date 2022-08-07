68fc40eb91f0eb3b7b2cfe5b960c43ee80d7b080
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-08-07 03:17:35 +0000

    Sync LeetCode submission - Check if There is a Valid Partition For The Array (cpp)

---
+++ AC-Submissions/problems/check_if_there_is_a_valid_partition_for_the_array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool validPartition(vector<int>& nums) {
+        int n = nums.size();
+        bool dp[n + 3];
+        for(int i = 0; i < nums.size(); ++i) {
+            dp[i] = false;
+        }
+        dp[n] = true;
+        
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] == nums[i + 1] and dp[i + 2]) dp[i] = true;
+            if(i + 2 < n and nums[i] == nums[i + 1] and nums[i] == nums[i + 2] and dp[i + 3]) dp[i] = true;
+            if(i + 2 < n and nums[i] + 1== nums[i + 1] and nums[i] + 2 == nums[i + 2] and dp[i + 3]) dp[i] = true;
+        }
+        return dp[0];
+    }
+};
