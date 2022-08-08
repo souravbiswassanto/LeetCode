d931f6e3e2dea6bb7312af2f283793eb7542bbc4
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-08-08 11:29:09 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

---
+++ AC-Submissions/problems/longest_increasing_subsequence/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> LIS;
+        for(auto num: nums) {
+            int id = lower_bound(LIS.begin(), LIS.end(), num) - LIS.begin();
+            if (id == LIS.size()) LIS.push_back(num);
+            else LIS[id] = num;
+        }
+        
+        return LIS.size();
+        
+    }
+};
