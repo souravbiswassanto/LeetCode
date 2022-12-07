e4e5d69019906c53f419fbb68bdc712181a2a29e
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-12-07 04:59:37 +0000

    Sync LeetCode submission - Range Sum of BST (cpp)

---
+++ AC-Submissions/problems/range_sum_of_bst/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root == NULL) return 0;
+        int a = rangeSumBST(root->left, low, high);
+        int b = rangeSumBST(root->right, low, high);
+        int sum = 0;
+        if (root->val >= low and root->val <= high) sum = root->val;
+        return a + b + sum;
+    }
+};
