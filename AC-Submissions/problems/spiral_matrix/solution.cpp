957ca7a7de7499c9455c7375f441504e03fad8f0
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-12-29 20:45:48 +0000

    Sync LeetCode submission - Spiral Matrix (cpp)

--- AC-Submissions/problems/spiral_matrix/solution.cpp
+++ AC-Submissions/problems/spiral_matrix/solution.cpp
@@ -2,48 +2,41 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n = matrix.size(); int m = matrix[0].size();
-        vector<vector<int>> newmat(n + 2, vector<int> (m + 2, 1)), Mat(n + 2, vector<int> (m + 2));
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                newmat[i][j] = 0;
-                Mat[i][j] = matrix[i - 1][j - 1];
-            }
-        }
-        int i = 1, j = 1; vector<int> v;
-        while(newmat[i][j] != 1){
-            
-            while(newmat[i][j] != 1){
-                v.pb(Mat[i][j]);
-                newmat[i][j] = 1;
-                j++;
-            }
-            j--; i++;
-            cout << i <<" "<< j << endl;
-            while(newmat[i][j] != 1){
-                v.pb(Mat[i][j]);
-                newmat[i][j] = 1;
-                i++;
-            }
-            i--; j--;
-            cout << i <<" "<< j << endl;
-            
-            while(newmat[i][j] != 1){
-                v.pb(Mat[i][j]);
-                newmat[i][j] = 1;
-                j--;
-            }
-            j++; i--;
-            cout << i <<" "<< j << endl;
-            while(newmat[i][j] != 1){
-                v.pb(Mat[i][j]);
-                newmat[i][j] = 1;
-                i--;
-            }
-            i++; j++;
-            cout << i <<" "<< j << endl;
-        }
-        return v;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int rmn = 0, rmx = n - 1;
+        int cmn = 0, cmx = m - 1;
+        vector<int> res;
         
+        while(rmn <= rmx and cmn <= cmx) {
+            if (rmx == rmn) {
+                for (int i = cmn; i <= cmx; i++) {
+                    res.pb(matrix[rmn][i]);
+                }
+            }
+            else if(cmx == cmn) {
+                for (int i = rmn; i <= rmx; i++){
+                    res.pb(matrix[i][cmn]);
+                }
+            }
+            else {
+                for (int i = cmn; i <= cmx; i++) {
+                    res.pb(matrix[rmn][i]);
+                }
+                for (int i = rmn + 1; i < rmx; i++){
+                    res.pb(matrix[i][cmx]);
+                }
+                for (int i = cmx; i >= cmn; i--) {
+                    res.pb(matrix[rmx][i]);
+                }
+                for (int i = rmx - 1; i > rmn; i--){
+                    res.pb(matrix[i][cmn]);
+                }
+            }
+            
+            rmn++, cmn++, rmx--, cmx--;
+            //for (auto it: res) cout << it <<" "; cout << endl;
+        }
+        return res;
     }
 };
