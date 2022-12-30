059cc7a274eefb308d6d88880ebf92464fd0af20
souravbiswassanto <48715910+souravbiswassanto@users.noreply.github.com>
2022-12-30 20:35:26 +0000

    Sync LeetCode submission - Longest Consecutive Sequence (cpp)

--- AC-Submissions/problems/longest_consecutive_sequence/solution.cpp
+++ AC-Submissions/problems/longest_consecutive_sequence/solution.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int, int> m1, m2;
+        unordered_map<int, int> hashMap, secondHashMap;
         int n = nums.size();
-        for(int i = 0; i < n; i++){
-            nums[i]++;
-            m1[nums[i]] = 1;
+        for (int i = 0; i < n; i++) {
+            hashMap[nums[i]] = 1;
         }
-        int res, mx = 0;
-        for(int i = 0; i < n; i++){
-            if(m2[nums[i]])continue;
-            res = nums[i];
-            for(int j = nums[i]; m1[j]; j++){
-                if(m2[j] != 0){
-                    res = m2[j]; break;
-                }
-                res = j;
+        for(int i = 0; i < n; i++) {
+            if(secondHashMap[nums[i]] != 0)continue;
+            int cntr = nums[i];
+            while(secondHashMap[cntr] == 0 and hashMap[cntr]) {
+                secondHashMap[nums[i]]++;
+                secondHashMap[cntr] = 1;
+                cntr++;
             }
-            m2[nums[i]] = res;
-            mx = max(mx, res - nums[i] + 1);
-            //cout << nums[i] <<" " << res << endl;
+            secondHashMap[nums[i]] += secondHashMap[cntr];
         }
-        return mx;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, secondHashMap[nums[i]]);
+        }
+        return ans;
     }
 };
